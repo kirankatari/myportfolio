9016c7900aced05fc27f10cfcc210ee37e822af8
k v n kiran <139694827+kirankatari@users.noreply.github.com>
2026-05-03 00:40:10 -0400

    Update config.local.php

--- api/config.local.php
+++ api/config.local.php
@@ -4,5 +4,5 @@
  * Never commit config.local.php to GitHub.
  */
 return [
-    'openai_api_key' => 'sk-proj-PASTE_YOUR_KEY_HERE',
+    'openai_api_key' => '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',
 ];
